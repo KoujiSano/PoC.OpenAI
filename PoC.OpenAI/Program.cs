b8d6f6b7a0bac4e713a1a550723b5dc086330d5b
佐野浩司 ( Kouji Sano ) <sano@simplise.jp>
2023-05-24 23:41:39 +0900

    update

--- PoC.OpenAI/Program.cs
+++ PoC.OpenAI/Program.cs
@@ -6,16 +6,10 @@ const string key = "";
 
 OpenAIAPI api = new OpenAIAPI(key);
 
+await api.Embeddings.CreateEmbeddingAsync("京都テックとは、京都デザイン＆テクノロジー専門学校は、２０２２年４月開校の新しい専門学校です。京都TECHとは、京都デザイン＆テクノロジー専門学校の略称です。");
+
+
 var chat = api.Chat.CreateConversation();
-
-/// give instruction as System
-// chat.AppendSystemMessage("You are a teacher who helps children understand if things are animals or not.  If the user tells you an animal, you say \"yes\".  If the user tells you something that is not an animal, you say \"no\".  You only ever respond with \"yes\" or \"no\".  You do not say anything else.");
-
-// give a few examples as user and assistant
-chat.AppendUserInput("京都テックとは？");
-chat.AppendExampleChatbotOutput("京都テックとは、京都デザイン＆テクノロジー専門学校は、２０２２年４月開校の新しい専門学校です。");
-chat.AppendUserInput("京都TECHとは？");
-chat.AppendExampleChatbotOutput("京都TECHとは、京都デザイン＆テクノロジー専門学校の略称です。");
 
 // now let's ask it a question'
 chat.AppendUserInput("京都テックとは？");
@@ -28,11 +22,5 @@ chat.AppendUserInput("京都TECHとは？");
 // and get another response
 response = await chat.GetResponseFromChatbotAsync();
 Console.WriteLine(response); // "No"
-
-// the entire chat history is available in chat.Messages
-foreach (var msg in chat.Messages)
-{
-    Console.WriteLine($"{msg.Role}: {msg.Content}");
-}
 
 
